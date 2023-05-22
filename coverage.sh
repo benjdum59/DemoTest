afeb743dbaa9e3c4bea8986a1728fd4b6fdb9f94
Benjamin Dumont <benjamin.dumont.pro@gmail.com>
2023-05-22 16:31:56 +0200

    coverage

--- coverage.sh
+++ coverage.sh
@@ -2,3 +2,15 @@
 
 slather coverage -x --output-directory report-output-dir --scheme DemoTest --workspace DemoTest.xcworkspace DemoTest.xcodeproj
 
+echo "Please provide Codacy token"
+read -s token
+
+export CODACY_API_TOKEN=$token
+export CODACY_ORGANIZATION_PROVIDER=gh
+export CODACY_USERNAME=benjdum59
+export CODACY_PROJECT_NAME=DemoTest
+
+echo "token:" $token
+
+bash <(curl -Ls https://coverage.codacy.com/get.sh) report -r report-output-dir/cobertura.xml
+
